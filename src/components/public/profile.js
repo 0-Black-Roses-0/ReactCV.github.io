fc034c4eb66e758e937ed613cb6d89cddeb8e6a6
0-Black-Roses-0 <92026627+0-Black-Roses-0@users.noreply.github.com>
2024-08-17 12:15:36 +0330

    update

---
+++ src/components/public/profile.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from '../style/profile.module.css'
+
+const profile =() =>{
+    return(
+        <div>
+            <div>
+                <img src="" alt="" />
+                <p>Mahan Rezaei</p>
+            </div>
+            
+            <div>
+                
+            </div>
+        </div>
+    );
+};
