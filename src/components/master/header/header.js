edf27aa0fb657fc451fcdb3176babe1720440290
0-Black-Roses-0 <92026627+0-Black-Roses-0@users.noreply.github.com>
2024-08-20 01:19:27 +0330

    update

--- src/components/master/header/header.js
+++ src/components/master/header/header.js
@@ -1,46 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from '../../style/header.module.css';
 
 function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
-        <ul className={styles.ulheader}>
-            <li className={styles.liheader}>
-                <NavLink
-                    to="/"
-                    className={({ isActive }) =>
-                        isActive ? `${styles.aheader} ${styles.active}` : styles.aheader} end>Home</NavLink>
-            </li>
+        <>
+            <ul className={styles.ulheadermobile}>
+                <p className={styles.liheadermobile}>Mahan Rrezaei</p>
+            </ul>
 
-            <li className={styles.liheader}>
-                <NavLink
-                    to="/dashboard"
-                    className={({ isActive }) =>
-                        isActive ? `${styles.aheader} ${styles.active}` : styles.aheader}>Dashbard</NavLink>
-            </li>
-            
-            <li className={styles.liheader}>
-                <NavLink
-                    to="/profile"
-                    className={({ isActive }) =>
-                        isActive ? `${styles.aheader} ${styles.active}` : styles.aheader}>Profile</NavLink>
-            </li>
+            {!menuOpen && (
+                <button className={styles.burgerButton} onClick={toggleMenu}>
+                    ☰
+                </button>
+            )}
 
-            <li className={styles.liheader}>
-                <NavLink
-                    to="/Notfound"
-                    className={({ isActive }) =>
-                        isActive ? `${styles.aheader} ${styles.active}` : styles.aheader}>404 not found</NavLink>
-            </li>
+            <ul className={`${styles.ulheader} ${menuOpen ? styles.menuOpen : ''}`}>
+                <li className={styles.liheader}>
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive ? `${styles.aheader} ${styles.active} ${styles.homeButton}` : `${styles.aheader} ${styles.homeButton}`} end>Home</NavLink>
+                </li>
 
-            <li className={`${styles.liheader} ${styles.liabout}`}>
-                <NavLink
-                    to="/login"
-                    className={({ isActive }) =>
-                        isActive ? `${styles.aheader} ${styles.active}` : styles.aheader}>Login</NavLink>
-            </li>
-
-        </ul>
+                <li className={styles.liheader}>
+                    <NavLink
+                        to="/dashboard"
+                        className={({ isActive }) =>
+                            isActive ? `${styles.aheader} ${styles.active}` : styles.aheader}>Dashboard</NavLink>
+                </li>
+                <li className={styles.liheader}>
+                    <NavLink
+                        to="/profile"
+                        className={({ isActive }) =>
+                            isActive ? `${styles.aheader} ${styles.active}` : styles.aheader}>Profile</NavLink>
+                </li>
+                <li className={styles.liheader}>
+                    <NavLink
+                        to="/Notfound"
+                        className={({ isActive }) =>
+                            isActive ? `${styles.aheader} ${styles.active}` : styles.aheader}>404 not found</NavLink>
+                </li>
+                <li className={`${styles.liheader} ${styles.liabout}`}>
+                    <NavLink
+                        to="/login"
+                        className={({ isActive }) =>
+                            isActive ? `${styles.aheader} ${styles.active}` : styles.aheader}>Login</NavLink>
+                </li>
+                <button className={styles.closeButton} onClick={toggleMenu}>✖</button>
+            </ul>
+        </>
     );
 }
 
